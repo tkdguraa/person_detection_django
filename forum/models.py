36df74be9531e384c07e375855d83be8c32bfe17
tkdguraa <1368798646@qq.com>
2018-09-11 13:21:55 +0800

    Remove useless code

--- forum/models.py
+++ forum/models.py
@@ -2,19 +2,3 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    url = models.CharField(max_length=200, blank=True, null=True)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-    keywords = models.TextField(blank=True, null=True)
-
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
--- forum/urls.py
+++ forum/urls.py
@@ -3,11 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/?$', views.post_detail, name='post_detail'),
-    url(r'^post/new/?$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/?$', views.post_edit, name='post_edit'),
-    url(r'^post/(?P<pk>\d+)/remove/?$', views.post_remove, name='post_remove'),
     url(r'^signup/?$', views.sign_up, name='signup'),
-    url(r'^get_news/?$', views.get_news, name='get_news'),
-    url(r'^results/$', views.search, name='search'),
 ]
